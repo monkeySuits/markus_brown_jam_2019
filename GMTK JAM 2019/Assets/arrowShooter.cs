679572d366cc0c3259f1392c8b585535b97b3d8e
RazorFinger <pedro.lopes0203@gmail.com>
2019-08-03 21:30:03 -0300

    Arrows and arrow spawners

---
+++ GMTK JAM 2019/Assets/arrowShooter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrowShooter : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject arrowPrefab;
+
+    public float timeToSpawn;
+    float curTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(curTime <= timeToSpawn)
+        {
+            curTime += Time.deltaTime;
+        }
+        else
+        {
+            SpawnArrow();
+            curTime = 0;
+        }
+    }
+
+    void SpawnArrow()
+    {
+        Instantiate(arrowPrefab, spawnPoints[Random.Range(0, spawnPoints.Length)]);
+    }
+}
